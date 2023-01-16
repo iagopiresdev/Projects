31b7476aeca53ec97c3838e0dd018614c63c4632
Iago Pires <iagopires.dev@gmail.com>
2023-01-16 13:14:03 -0400

    Started to create version 2.0

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,15 +1,21 @@
-let todo = document.getElementById('task');
-let button = document.getElementById('add');
+let task1 = document.getElementById('task1');
+let task1Text = document.getElementById('task1Text');
+let task2 = document.getElementById('task2');
+let task3 = document.getElementById('task3');
 
-function addTask(){
-    let task = todo.value;
-    let list = document.getElementById('tasks');
-    let li = document.createElement('li');
-    li.textContent = task;
-    list.append(li);
-    todo.value = "";
-}
 
-if(button){
-    button.addEventListener('click', addTask);
+/*task1.addEventListener('change', checkTask);*/
+function checkTask() {
+    if (task1.checked){
+        let newTask = document.createElement('s'); 
+        newTask.textContent = task1Text.textContent;
+        task1Text.replaceWith(newTask);
+        window.alert('Task 1 is completed');
+    }else{
+        let newTask = null;
+        newTask = document.createElement('p');
+        newTask.textContent = task1Text.textContent;
+        task1Text.replaceWith(newTask);
+        window.alert('Task 1 is not completed');
+    }
 }

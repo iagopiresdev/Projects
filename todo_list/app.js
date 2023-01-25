8ec85e2079b60dc19a09f1a7d0770a85ffd501b9
Iago Pires <iagopires.dev@gmail.com>
2023-01-24 20:31:47 -0400

    minor updates

--- todo_list/app.js
+++ todo_list/app.js
@@ -21,25 +21,43 @@ function add(){
     let li = document.createElement('li');
     let checkbox = document.createElement('input');
     let label = document.createElement('label');
-    let deleteButton = document.createElement('button');
+    //let deleteButton = document.createElement('button');
 
     li.appendChild(checkbox);
     li.appendChild(label);
-    li.appendChild(deleteButton);
+    //li.appendChild(deleteButton);
     list.appendChild(li);
 
     label.innerText = text;
     checkbox.value = undefined;
-    deleteButton.innerText = 'Delete';
+    //deleteButton.innerText = 'Delete';
 
     checkbox.type = 'checkbox';
     checkbox.className = 'checkboxes';
     label.className = 'label';
-    deleteButton.className = 'delete';
+    //deleteButton.className = 'delete';
 
     document.getElementById('add').value = '';
+    counter();
 }
 
+function counter(){
+    let labels = document.querySelectorAll('label');
+    let counter = document.getElementById('counter');
+    counter.innerText = 'You have ' + labels.length + ' tasks left.';
+}
+
+function deleteAll(){
+    let list = document.getElementById('list');
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            list.removeChild(checkboxes[i].parentNode);
+            i--;
+        }
+    }
+    counter();
+}
 
 /*    deleteButton.addEventListener('click', deleteTask);
     checkbox.addEventListener('change', checkTask);
